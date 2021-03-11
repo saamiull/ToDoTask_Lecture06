e053eb75f2e823c6ff6fa97b93a1fe1666301f29
sami2905 <bsef18a028@pucit.edu.pk>
2021-03-11 17:35:50 +0500

    First TO DO Task

---
+++ MyApplication/app/src/main/java/org/samiulhaq/myapplication/MainActivity2.java
@@ -0,0 +1,31 @@
+package org.samiulhaq.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+
+    public void ShowWebsite(View view) {
+        Uri uri = Uri.parse("http://www.pucit.edu.pk");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void Call(View view) {
+        Uri uri = Uri.parse("tel:+92334037449");
+        Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(intent);
+
+    }
+}
